6f79f09a6c6700237e59a1473f038e603fdb2ab7
knaeeim <mdkhairulb01@gmail.com>
2025-12-28 23:42:33 +0600

    Better Auth Additional Feilds add and user creation setup done for better auth

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express, { Application, Request, Response } from 'express';
 import { postRouter } from './modules/post/post.routes';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
 
 const app: Application = express();
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+
 app.use(express.json());
 
 app.use('/posts', postRouter);
--- src/modules/post/post.routes.ts
+++ src/modules/post/post.routes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, response, Router } from "express";
+import { Router } from "express";
 import { postController } from "./post.controller";
 
 const router = Router();
