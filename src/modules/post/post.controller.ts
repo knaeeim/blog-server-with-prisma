5fba90f0c5b7b538dc459f81cbf0a2830acf988c
knaeeim <mdkhairulb01@gmail.com>
2026-01-03 00:44:04 +0600

    When pagination value is not passed that time it was giving error, solved it now

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -24,8 +24,8 @@ const getAllPosts = async (req: Request, res: Response) => {
         const tags = req.query.tags ? (req.query.tags as string).split(',') : [];
         const { isFeatured } = req.query;
         const isFeaturedBool = isFeatured === 'true' ? true : isFeatured === 'false' ? false : undefined;
-        const page = Number(req.query.page) ?? 1;
-        const limit = Number(req.query.limit) ?? 10;
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
         const result = await postServices.getAllPosts({ searchTerm, tags, isFeaturedBool, page, limit });
         if (result.length === 0) {
             res.status(200).json({
